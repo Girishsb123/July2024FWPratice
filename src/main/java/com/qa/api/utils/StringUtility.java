22ca1d6dd5f1325c7ca3af7dbc91c71ed6a2369a
Girish <girishvjc1993@gmail.com>
2024-10-26 11:07:41 +0530

    added api frameworkcode

---
+++ src/main/java/com/qa/api/utils/StringUtility.java
@@ -0,0 +1,10 @@
+package com.qa.api.utils;
+
+public class StringUtility {
+	
+	public static String getRandomEmailId() {
+		
+		return "apiautomation"+System.currentTimeMillis()+"@Open.com";
+	}
+
+}
